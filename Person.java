de6127fbbd5bd6c04c748c8f92827295ee302297
1U23CA038GOKULS <139437434+1U23CA038GOKULS@users.noreply.github.com>
2024-02-14 12:05:00 +0000

    Eren

--- Person.java
+++ Person.java
@@ -1,29 +1,25 @@
-class Person1
-{
+class Person1{
     String FirstName;
     String LastName;
     int Age;
-Person1(String fn,String ln,int a)
-{
+Person1(String fn,String ln,int a){
     FirstName = fn;
     LastName = ln;
     Age = a;
 }
-String GetFullName()
-{
+String GetFullName(){
     return FirstName + " "+LastName;
 }
 }
-public class Person
-{
-    public static void main(String[] args)
-    {
-        Person1 a1 = new Person1("Jhon","Doe",30);
-        Person1 a2 = new Person1("Alice","Smith",25);
-        System.out.println("Person 1 : " + a1.GetFullName());
-        System.out.println("Person 2 : " + a2.GetFullName());
-        double sum = a1.Age + a2.Age;
-        double avg = sum/2;
-        System.out.println("Average Age : " + avg);
+public class Person{
+    public static void main(String[] args){
+        Person1 ob1 = new Person1("Jhon","Doe",30);
+        Person1 ob2 = new Person1("Alice","Smith",25);
+        System.out.println("Person 1:"+ob1.GetFullName());
+        System.out.println("Person 2:"+ob2.GetFullName());
+
+        double sum = ob1.Age + ob2.Age;
+        double average = sum/2;
+        System.out.println("Average Age:"+average);
     }
 }
